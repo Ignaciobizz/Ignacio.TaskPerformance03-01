c91f414047a1f9bece1b3b39cab3880eaf94b96c
Ignaciobizz <allenjustine23@gmail.com>
2026-02-07 17:19:45 +0800

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,26 +1,24 @@
 ﻿using System;
 
-namespace ComputeAverageApp
+namespace MathApp
 {
-    class ComputeAverageProgram
+    class MathProgram
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter 5 grades separated by new line:");
 
-            double g1 = Convert.ToDouble(Console.ReadLine());
-            double g2 = Convert.ToDouble(Console.ReadLine());
-            double g3 = Convert.ToDouble(Console.ReadLine());
-            double g4 = Convert.ToDouble(Console.ReadLine());
-            double g5 = Convert.ToDouble(Console.ReadLine());
+            double firstNum = 15.75;
+            double secondNum = -20.0;
+            double myBase = 2.0;
+            double myPower = 3.0;
 
-            double average = (g1 + g2 + g3 + g4 + g5) / 5;
+            Console.WriteLine("Absolute value of " + secondNum + " is: " + Math.Abs(secondNum));
+            Console.WriteLine("Higher value between " + firstNum + " and 10 is: " + Math.Max(firstNum, 10));
+            Console.WriteLine(myBase + " to the power of " + myPower + " is: " + Math.Pow(myBase, myPower));
+            Console.WriteLine("Square root of 144 is: " + Math.Sqrt(144));
+            Console.WriteLine("Ceiling value of " + firstNum + " is: " + Math.Ceiling(firstNum));
 
-            double roundedAverage = Math.Round(average);
-
-            Console.WriteLine("The average is " + average + " and round off to " + roundedAverage);
-
-            Console.WriteLine("Press any key to exit...");
+            Console.WriteLine("\nPress any key to exit...");
             Console.ReadKey();
         }
     }
